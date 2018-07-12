765c33738c91bb5ba59277c9f15cdc749be4f226
233705835@qq.com <233705835@qq.com>
2018-07-12 10:27:13 +0800

    add mock-heroes.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HERO } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
+  
+  heroes = HERO;
+
+  // 把该组件的 hero 属性改名为 selectedHero，但不要为它赋值,
+  // 它会把模板中被点击的英雄赋值给组件的 selectedHero 属性。Hero 已被改名为：selectedHero
+  selectedHero: Hero;   //undefined
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // 添加click事件:void 不返回;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
